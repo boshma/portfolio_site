9bf3c9ee2854eaed0af4619ad78a6ac7c66fe018
Bogdan Shmat <Bogdan.shmat@invent.us>
2023-08-18 23:39:02 -0700

    fix: particles behind navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,13 @@
+//src/components/NavBar.tsx
 import React from 'react';
 import { Link as ScrollLink } from 'react-scroll';
 
 const NavBar = ({ navBottom }: { navBottom: boolean }) => {
   const navPosition = navBottom ? 'bottom-0' : 'top-0';
+  const bgColor = navBottom ? 'bg-transparent' : 'bg-gray-900';
+  
   return (
-    <nav className={`fixed ${navPosition} left-0 w-full bg-gray-900 text-white z-50 transition-all duration-300 ease-in-out backdrop-filter backdrop-blur-lg`}>
+    <nav className={`fixed ${navPosition} left-0 w-full ${bgColor} text-white z-50 transition-all duration-300 ease-in-out`}>
       <div className="container mx-auto">
         <ul className="flex justify-around py-4">
           <li><ScrollLink to="tech" smooth={true}>Tech</ScrollLink></li>
