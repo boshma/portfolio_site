cfd9a64451480c7f894f8c7a539ac2867f7bd3b1
Bogdan Shmat <Bogdan.shmat@invent.us>
2023-08-20 21:43:59 -0700

    add icons

---
+++ src/components/TechSection.tsx
@@ -0,0 +1,30 @@
+//src/components/TechSection.tsx
+import { Icon } from '@iconify/react';
+import React from 'react';
+
+const TechSection = () => {
+  const techItems = [
+    { icon: "logos:react", name: "React" },
+    { icon: "vscode-icons:file-type-light-prisma", name: "Prisma" },
+    { icon: "logos:trpc", name: "tRPC" },
+    { icon: "devicon:tailwindcss", name: "Tailwind CSS" },
+    { icon: "logos:planetscale", name: "PlanetScale" },
+    { icon: "logos:nextjs-icon", name: "Next.js" },
+    { icon: "logos:typescript-icon", name: "TypeScript" },
+    { icon: "skill-icons:styledcomponents", name: "Styled Components" },
+  ];
+
+  return (
+    <div id="tech" className="flex flex-wrap space-x-0 p-8">
+      {techItems.map((item, index) => (
+        <div key={index} className="flex flex-col items-center w-1/4 p-4">
+          <Icon icon={item.icon} className="w-20 h-20" /> 
+          <p className="mt-2">{item.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TechSection;
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import HeartSVG from '~/components/heartSVG';
 import Particles from '~/components/Particles';
 import NavBar from '~/components/NavBar';
 import { useEffect, useState } from 'react';
+import TechSection from '~/components/TechSection';
 
 const Home = () => {
   const [navBottom, setNavBottom] = useState(true);
@@ -33,11 +34,15 @@ const Home = () => {
           <Particles id="tsparticles" />
         </div>
         <div className="flex items-center z-10">
-          <h2 className="text-4xl mr-2">I</h2>  
-          <HeartSVG className="h-10 w-10 mr-2"/> 
+          <h2 className="text-4xl mr-2">I</h2>
+          <HeartSVG className="h-10 w-10 mr-2" />
         </div>
         <TypingText className="z-10" />
+
       </main>
+      <div id="tech">
+        <TechSection />
+      </div>
     </>
   );
 };
