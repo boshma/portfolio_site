5ff3db250b1f0c59e5978040d3f0cc503f9f1354
Bogdan Shmat <Bogdan.shmat@invent.us>
2023-08-23 23:40:49 -0700

    pointer and color

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,15 +5,15 @@ import { Link as ScrollLink } from 'react-scroll';
 const NavBar = ({ navBottom }: { navBottom: boolean }) => {
   const navPosition = navBottom ? 'bottom-0' : 'top-0';
   const bgColor = navBottom ? 'bg-transparent' : 'bg-gray-900';
-  
+
   return (
     <nav className={`fixed ${navPosition} left-0 w-full ${bgColor} text-white z-50 transition-all duration-300 ease-in-out`}>
       <div className="container mx-auto">
         <ul className="flex justify-around py-4">
-          <li><ScrollLink to="tech" smooth={true}>Tech</ScrollLink></li>
-          <li><ScrollLink to="experience" smooth={true}>Experience</ScrollLink></li>
-          <li><ScrollLink to="showcase" smooth={true}>Showcase</ScrollLink></li>
-          <li><ScrollLink to="contact" smooth={true}>Contact</ScrollLink></li>
+          <li className="cursor-pointer"><ScrollLink to="tech" smooth={true}>Tech</ScrollLink></li>
+          <li className="cursor-pointer"><ScrollLink to="experience" smooth={true}>Experience</ScrollLink></li>
+          <li className="cursor-pointer"><ScrollLink to="showcase" smooth={true}>Showcase</ScrollLink></li>
+          <li className="cursor-pointer"><ScrollLink to="contact" smooth={true}>Contact</ScrollLink></li>
         </ul>
       </div>
     </nav>
--- src/components/ShowcaseSection.tsx
+++ src/components/ShowcaseSection.tsx
@@ -8,8 +8,8 @@ const ShowcaseSection = () => {
       <h2 className="text-4xl font-bold text-blue-400 mb-4 italic tracking-widest">(TypeScript, Next.js, React, Prisma, PlanetScale, Trpc, Shadcn, Tailwind.css, Clerk)</h2>
       <div className="flex justify-center">
         <div className="w-1/2 p-4">
-          <p className="text-2xl font-semibold mb-4">All in one Fitness App!</p>
-          <ul className="text-lg font-serif">
+          <p className="text-4xl text-blue-200 font-semibold mb-4">All in one Fitness App!</p>
+          <ul className="text-2xl text-blue-200 font-serif">
             <li>Quick add meals</li>
             <li>Save meals to your collection and add from there!</li>
             <li>Add meals from nutritionix database</li>
@@ -17,8 +17,8 @@ const ShowcaseSection = () => {
           </ul>
         </div>
         <div className="w-1/2 p-4">
-          <p className="text-2xl font-semibold mb-4">TODO</p>
-          <ul className="text-lg font-serif">
+          <p className="text-4xl font-semibold mb-4 text-blue-200">TODO</p>
+          <ul className="text-2xl text-blue-200 font-serif">
             <li>Migrate fully to server components</li>
             <li>Websocket live chat using Pusher API</li>
             <li>Friend/Trainer/Client roles</li>
@@ -26,7 +26,7 @@ const ShowcaseSection = () => {
           </ul>
         </div>
       </div>
-      <p className="text-2xl font-semibold mb-4"> <a target="_blank" href="https://github.com/boshma">Check out my other projects in Kotlin, Java and Django</a> </p>
+      <p className="text-2xl font-semibold mb-4 text-blue-800"> <a target="_blank" href="https://github.com/boshma">Check out my other projects in Kotlin, Java and Django</a> </p>
     </div>
   );
 };
