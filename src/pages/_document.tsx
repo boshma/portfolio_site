ea36097c708812443b2ad175fcb38a2e1fa878dc
Bogdan Shmat <Bogdan.shmat@invent.us>
2023-12-02 12:46:12 -0800

    fonts

---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+// pages/_document.tsx
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="preload"
+            href="https://fonts.googleapis.com/css?family=Fredericka+the+Great|Frijole"
+            as="style"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Fredericka+the+Great|Frijole"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
