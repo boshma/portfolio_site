76ca08ec2f8298a8efcae153131ad31cf7b4a39f
Bogdan Shmat <Bogdan.shmat@invent.us>
2023-12-04 09:18:27 -0800

    canical dude

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Fredericka+the+Great|Frijole"
             rel="stylesheet"
           />
+            <link rel="canonical" href="https://www.bogdan-shmat-portfolio.com/" />
         </Head>
         <body>
           <Main />
