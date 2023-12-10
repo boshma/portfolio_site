e8b80343d5890f3a89a3031b2905db3514b3b595
Bogdan Shmat <Bogdan.shmat@invent.us>
2023-12-09 16:56:30 -0800

    speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
-import '../styles/stars.scss';
-import '../styles/snow.scss';
-import '~/styles/christmas-lights.scss';
-
-
+import "../styles/stars.scss";
+import "../styles/snow.scss";
+import "~/styles/christmas-lights.scss";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <SpeedInsights />
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
