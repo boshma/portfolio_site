03725e7f9778a4d54e265758fc95e281bece16bb
Bogdan Shmat <bogdan.shmat@wsu.edu>
2024-03-16 10:59:33 -0700

    cooking

--- src/components/typingtext.tsx
+++ src/components/typingtext.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import type { FunctionComponent } from "react";
 import ChristmasLights from "./ChristmasLights";
 
-const textList = ['ice cream', 'sleep', 'nature', 'blueberries', 'walks', 'meeting new ppl', 'biotracking', 'coffee'];
+const textList = ['ice cream', 'sleep', 'nature', 'blueberries', 'walks', 'cooking', 'coffee'];
 
 interface TypingTextProps {
   className?: string;
