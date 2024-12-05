c79a6c8351ca23b37e86278680febadbdcf817ca
Bogdan Shmat <bogdan.shmat@wsu.edu>
2024-12-05 10:20:43 -0800

    fix navbar, reopen bug with too much xoferflow

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ import { Link as ScrollLink } from 'react-scroll';
 const NavBar = ({ navBottom, visible }: { navBottom: boolean, visible: boolean }) => {
   const navStyle: CSSProperties = {
     position: 'sticky',
-    top: navBottom ? 'unset' : 0, 
+    top: navBottom ? 'unset' : 0,
     bottom: navBottom ? 0 : 'unset',
     backgroundColor: 'transparent',
     zIndex: 50,
@@ -19,17 +19,25 @@ const NavBar = ({ navBottom, visible }: { navBottom: boolean, visible: boolean }
   };
 
 
-   // Apply classes conditionally based on the `visible` prop
-   const navClasses = `transition-opacity duration-500 ease-in-out ${visible ? 'opacity-100' : 'opacity-0'}`;
+  // Apply classes conditionally based on the `visible` prop
+  const navClasses = `transition-opacity duration-500 ease-in-out ${visible ? 'opacity-100' : 'opacity-0'}`;
 
   return (
-    <nav style={navStyle}  className={navClasses}>
+    <nav style={navStyle} className={navClasses}>
       <div className="container mx-auto">
         <ul className="flex justify-around py-4 space-x-2">
-          <li className="cursor-pointer"><ScrollLink to="tech" smooth={true} className="scroll-link">Tech</ScrollLink></li>
-          <li className="cursor-pointer"><ScrollLink to="experience" smooth={true} className="scroll-link">Experience</ScrollLink></li>
-          <li className="cursor-pointer"><ScrollLink to="showcase" smooth={true} className="scroll-link">Showcase</ScrollLink></li>
-          <li className="cursor-pointer"><ScrollLink to="contact" smooth={true} className="scroll-link">Contact</ScrollLink></li>
+          <li className="cursor-pointer">
+            <ScrollLink to="tech" smooth={true} duration={500} className="scroll-link">Tech</ScrollLink>
+          </li>
+          <li className="cursor-pointer">
+            <ScrollLink to="experience" smooth={true} duration={500} className="scroll-link">Experience</ScrollLink>
+          </li>
+          <li className="cursor-pointer">
+            <ScrollLink to="showcase" smooth={true} duration={500} className="scroll-link">Showcase</ScrollLink>
+          </li>
+          <li className="cursor-pointer">
+            <ScrollLink to="contact" smooth={true} duration={500} className="scroll-link">Contact</ScrollLink>
+          </li>
         </ul>
       </div>
     </nav>
